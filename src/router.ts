d92abcc396bcd4747d028c4a2e59dd3889719d22
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-12-10 00:18:13 -0600

    I added validation when register

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,29 @@
 import { Router } from "express";
 import { createAccount } from "./handlers";
+import { body } from "express-validator";
 
 const router = Router();
 
+/** Autenticacion y Registro */
+router.post('/auth/register',
+    body('handle')
+        .notEmpty()
+        .withMessage('El handle no puede ir vacio'),
+    body('name')
+        .notEmpty()
+        .withMessage('El Nombre no puede ir vacio'),
+    body('email')
+        .isEmail()
+        .withMessage('E-mail no válido'),
+    body('password')
+        .isLength({ min: 8 })
+        .withMessage('El Password es muy corto, mínimo 8 caracteres'),
 
-router.post("/auth/register", createAccount);
+    createAccount
+)
+
+router.post('/auth/login',
+
+)
 
 export default router;  
